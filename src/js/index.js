7761029b061bb18171e35d5c56873300150086d2
Ivan Shalnov <ivan.shalnov.94@gmail.com>
2022-09-04 21:50:27 +0200

    add sects

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,24 @@
-import Splide from '@splidejs/splide';
-import '@splidejs/splide/dist/css/splide-core.min.css';
-new Splide('.splide', {
-  classes: {
-    arrows: 'splide__arrows our-drones__arrows',
-    arrow: 'splide__arrow our-drones__arrow',
-    prev: 'splide__arrow--prev our-drones__prev',
-    next: 'splide__arrow--next our-drones__next',
+// import Swiper JS
+import Swiper, { Navigation } from 'swiper';
+
+Swiper.use([Navigation]);
+// import Swiper styles
+import 'swiper/swiper.min.css';
+const containerOffset = document.querySelector('.container').offsetLeft;
+
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper__btn-next',
+    prevEl: '.swiper__btn-prev',
+    disabledClass: 'swiper__btn-disabled',
   },
-}).mount();
+  spaceBetween: 15,
+  breakpoints: {
+    1200: {
+      slidesPerView: 1.57,
+      slidesOffsetBefore: containerOffset,
+      slidesOffsetAfter: containerOffset,
+      spaceBetween: 100,
+    },
+  },
+});
